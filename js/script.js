2b08e917ae345274b3c4479b2c185c7c0d4a6544
LuizHNR <luizhneri12@gmail.com>
2024-08-07 09:48:38 -0300

    arrays

--- js/script.js
+++ js/script.js
@@ -22,30 +22,43 @@
 //console.log(arrObjFrutas);
 
 //Integrando arrays com MAP
-let frutas = [
-    {"id":1, "nmFruta":"Jaca"},
-    {"id":2, "nmFruta":"Carambola"},
-    {"id":3, "nmFruta":"Zimbro"},
-    {"id":4, "nmFruta":"Mirtilo"},
-    {"id":5, "nmFruta":"Lichia"},
-];
+// let frutas = [
+//     {"id":1, "nmFruta":"Jaca"},
+//     {"id":2, "nmFruta":"Carambola"},
+//     {"id":3, "nmFruta":"Zimbro"},
+//     {"id":4, "nmFruta":"Mirtilo"},
+//     {"id":5, "nmFruta":"Lichia"},
+// ];
 
-let precos = [
-    {"id":1, "preco":5.89},
-    {"id":2, "preco":7.59},
-    {"id":3, "preco":18.19},
-    {"id":4, "preco":15.49},
-    {"id":5, "preco":5.44},
-];
+// let precos = [
+//     {"id":1, "preco":5.89},
+//     {"id":2, "preco":7.59},
+//     {"id":3, "preco":18.19},
+//     {"id":4, "preco":15.49},
+//     {"id":5, "preco":5.44},
+// ];
 
-let frutasPrecificadas = frutas.map((fruta)=>{
+// let frutasPrecificadas = frutas.map((fruta)=>{
 
-    //Realizando uma busca no objeto fruta com o objeto precos
-    let preco = precos.find(preco=> preco.id === fruta.id); 
-    return {...fruta, preco:preco ? preco.preco:null};
-    // é o msm que
-    //return {fruta:fruta.id,fruta:fruta.nmFruta, preco:preco ? preco.preco:null};
+//     //Realizando uma busca no objeto fruta com o objeto precos
+//     let preco = precos.find(preco=> preco.id === fruta.id); 
+//     return {...fruta, preco:preco ? preco.preco:null};
+//     // é o msm que
+//     //return {fruta:fruta.id,fruta:fruta.nmFruta, preco:preco ? preco.preco:null};
 
+// });
+
+// console.log(frutasPrecificadas);
+
+let numeros = [1,2,3,4,5];
+
+// numeros.forEach( (numero)=> {
+//     numero += numero;
+//     console.log(numero);
+// });
+
+const soma = numeros.reduce((acumulado,valAtual)=> { 
+   return acumulado+valAtual 
 });
 
-console.log(frutasPrecificadas);
+console.log(soma);
