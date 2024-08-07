882135e2b4a88711b9c314cce3e158ef0ecc7d04
irissuu <iriswinx2@gmail.com>
2024-08-07 09:55:41 -0300

    atividade

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //Declarando um array com 5 frutas.
 // let frutas = [
 //     "Jaca","Carambola","Zimbro","Mirtilo","Lichia"
@@ -36,7 +35,7 @@ let precos = [
     {"id":2, "preco":7.56},
     {"id":3, "preco":10.14},
     {"id":4, "preco":17.56},
-    {"id":5, "preco":12.33},
+    {"id":5, "preco":6.33},
 ];
 
 let frutasPrecificadas = frutas.map((fruta)=>{
@@ -44,6 +43,35 @@ let frutasPrecificadas = frutas.map((fruta)=>{
     let preco = precos.find(preco=> preco.id === fruta.id);
     return {...fruta, preco:preco ? preco.preco:null};
     //return {fruta:fruta.id,fruta:fruta.nmFruta, preco:preco ? preco.preco:null};
-});
+})
+// }).filter() ;
 
 console.log(frutasPrecificadas);
+
+//Filtrar as frutas com preço menor que 7;
+const frutasAbaixoDoPreco = frutasPrecificadas.filter((fruta)=> fruta.preco <= 7);
+console.log(frutasAbaixoDoPreco);
+
+//Exemplo simples de acumulação de valores do array com FOR EACH
+
+// let numeros = [1,2,3,4,5];
+
+// numeros.forEach( (numero)=> {
+//     numero += numero;
+//     acumulado = numero;
+//     console.log(acumulado);
+// });
+
+// for (let x = 0; x < numeros.length; ++x) {
+//     numeros[x] += numeros[x];
+//     acumulado = numeros[x];
+// }
+
+// console.log(acumulado);
+
+// const soma = numeros.reduce((acumulado,valAtual)=>{ 
+//     return acumulado+valAtual;
+// });
+
+// console.log(soma);
+
